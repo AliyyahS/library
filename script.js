3efddf7d8c7b89d9eec909188e48d9d5daaa380f
AliyyahS <50198558+AliyyahS@users.noreply.github.com>
2024-01-17 18:05:27 +0000

    Add functions to store books in array and display books on page

---
+++ script.js
@@ -0,0 +1,52 @@
+const library = []
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary(book) {
+    library.push(book)
+}
+
+function displayBook(book) {
+  const main = document.querySelector('.main')
+  
+  const card = document.createElement('div')
+  card.classList.add('card')
+  main.appendChild(card)
+
+  const info = document.createElement('div')
+  info.classList.add('info')
+  card.appendChild(info)
+
+  const titleDisplay = document.createElement('h1')
+  info.appendChild(titleDisplay)
+  titleDisplay.textContent = book.title
+
+  const authorDisplay = document.createElement('h2')
+  info.appendChild(authorDisplay)
+  authorDisplay.textContent = `by ${book.author}`
+
+  const pageDisplay = document.createElement('h3')
+  info.appendChild(pageDisplay)
+  pageDisplay.textContent = `${book.pages} pages`
+
+  const controls = document.createElement('div')
+  controls.classList.add('controls')
+  card.appendChild(controls)
+
+  const status = document.createElement('div')
+  status.classList.add('status')
+  controls.appendChild(status)
+  if (book.read) {
+    status.textContent = 'Read'
+  } else status.textContent = 'Unread'
+
+  const deleteBtn = document.createElement('div')
+  deleteBtn.classList.add('delete')
+  controls.appendChild(deleteBtn)
+  deleteBtn.textContent = 'Delete'
+}
