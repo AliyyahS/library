4d1c2ce1f7d995159fd1afb5869d97a08d022f83
AliyyahS <50198558+AliyyahS@users.noreply.github.com>
2024-01-18 12:42:33 +0000

    Add modal dialog box to get input from user

--- script.js
+++ script.js
@@ -14,39 +14,64 @@ function addBookToLibrary(book) {
 function displayBook(book) {
   const main = document.querySelector('.main')
   
-  const card = document.createElement('div')
-  card.classList.add('card')
-  main.appendChild(card)
+  library.forEach(function(book) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    main.appendChild(card)
 
-  const info = document.createElement('div')
-  info.classList.add('info')
-  card.appendChild(info)
+    const info = document.createElement('div')
+    info.classList.add('info')
+    card.appendChild(info)
 
-  const titleDisplay = document.createElement('h1')
-  info.appendChild(titleDisplay)
-  titleDisplay.textContent = book.title
+    const titleDisplay = document.createElement('h1')
+    info.appendChild(titleDisplay)
+    titleDisplay.textContent = book.title
 
-  const authorDisplay = document.createElement('h2')
-  info.appendChild(authorDisplay)
-  authorDisplay.textContent = `by ${book.author}`
+    const authorDisplay = document.createElement('h2')
+    info.appendChild(authorDisplay)
+    authorDisplay.textContent = `by ${book.author}`
 
-  const pageDisplay = document.createElement('h3')
-  info.appendChild(pageDisplay)
-  pageDisplay.textContent = `${book.pages} pages`
+    const pageDisplay = document.createElement('h3')
+    info.appendChild(pageDisplay)
+    pageDisplay.textContent = `${book.pages} pages`
 
-  const controls = document.createElement('div')
-  controls.classList.add('controls')
-  card.appendChild(controls)
+    const controls = document.createElement('div')
+    controls.classList.add('controls')
+    card.appendChild(controls)
 
-  const status = document.createElement('div')
-  status.classList.add('status')
-  controls.appendChild(status)
-  if (book.read) {
-    status.textContent = 'Read'
-  } else status.textContent = 'Unread'
+    const status = document.createElement('div')
+    status.classList.add('status')
+    controls.appendChild(status)
+    if (book.read) {
+      status.textContent = 'Read'
+    } else status.textContent = 'Unread'
 
-  const deleteBtn = document.createElement('div')
-  deleteBtn.classList.add('delete')
-  controls.appendChild(deleteBtn)
-  deleteBtn.textContent = 'Delete'
+    const deleteBtn = document.createElement('div')
+    deleteBtn.classList.add('delete')
+    controls.appendChild(deleteBtn)
+    deleteBtn.textContent = 'Delete'
+  })
 }
+
+// DOM elements
+
+const addBookBtn = document.querySelector('#addBook')
+
+const dialog = document.querySelector('dialog')
+const submitBtn = dialog.querySelector('#submit')
+const cancelBtn = dialog.querySelector('#cancel')
+
+// Event listeners
+
+addBookBtn.addEventListener("click", () => {
+  dialog.showModal()
+});
+
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault()
+  dialog.close()
+});
+
+cancelBtn.addEventListener("click", () => {
+  dialog.close()
+});
